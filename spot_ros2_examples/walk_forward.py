a72eb566aab2bf32c7edfa120f82f152d6713f10
Willie McClinton <wbm3.important@gmail.com>
2023-05-23 11:47:49 -0400

    fixed autoformat

--- spot_ros2_examples/walk_forward.py
+++ spot_ros2_examples/walk_forward.py
@@ -1,7 +1,8 @@
 """Simple Spot ROS2 Example in Predicators Repo."""
 
 import rclpy  # pylint: disable=import-error
-from simple_walk_forward.walk_forward import WalkForward  # pylint: disable=import-error
+from simple_walk_forward.walk_forward import \
+    WalkForward  # pylint: disable=import-error
 
 rclpy.init()
 goto = WalkForward()
