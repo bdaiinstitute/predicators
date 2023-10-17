bc245225209f0d34cea28b14ef825d96f9cb7ff9
Tom Silver <tsilver@theaiinstitute.com>
2023-10-17 10:05:02 -0400

    remove mess

--- predicators/spot_utils/skills/spot_find_objects.py
+++ predicators/spot_utils/skills/spot_find_objects.py
@@ -30,7 +30,6 @@ def _find_objects_with_choreographed_moves(
     relative_base_moves: List[math_helpers.SE2Pose],
     relative_hand_moves: Optional[List[math_helpers.SE3Pose]] = None,
     open_and_close_gripper: bool = True,
-    use_gui: bool = False,
 ) -> Tuple[Dict[ObjectDetectionID, math_helpers.SE3Pose], Dict[str, Any]]:
     """Helper for object search with hard-coded relative moves."""
 
@@ -54,26 +53,6 @@ def _find_objects_with_choreographed_moves(
     all_detections.update(detections)
     all_artifacts.update(artifacts)
 
-    # Display the detections on screen so that we can follow along.
-    if use_gui:
-        num_cameras = len(rgbds)
-        num_display_rows = int(np.ceil(np.sqrt(num_cameras)))
-        num_display_cols = int(np.ceil(num_cameras / num_display_rows))
-        display_fig_scale = 10
-        fig, display_axes = plt.subplots(
-            num_display_rows,
-            num_display_cols,
-            squeeze=False,
-            figsize=(display_fig_scale * num_display_rows,
-                     display_fig_scale * num_display_cols))
-
-        plt.ion()
-        plt.show()
-        plt.pause(0.1)
-        display_camera_detections(artifacts, display_axes)
-        fig.canvas.draw()
-        plt.pause(0.1)
-
     for i, relative_pose in enumerate(relative_base_moves):
         remaining_object_ids = set(object_ids) - set(all_detections)
         print(f"Found objects: {set(all_detections)}")
@@ -96,15 +75,6 @@ def _find_objects_with_choreographed_moves(
         detections, artifacts = detect_objects(object_ids, rgbds)
         all_detections.update(detections)
         all_artifacts.update(artifacts)
-
-        # Update the GUI.
-        if use_gui:
-            display_camera_detections(artifacts, display_axes)
-            fig.canvas.draw()
-            plt.pause(0.1)
-
-    # Stop the display.
-    plt.close()
 
     # Close the gripper.
     if open_and_close_gripper:
@@ -146,8 +116,7 @@ def init_search_for_objects(
         localizer,
         object_ids,
         base_moves,
-        relative_hand_moves=relative_hand_moves,
-        use_gui=True)
+        relative_hand_moves=relative_hand_moves)
 
 
 def find_objects(
