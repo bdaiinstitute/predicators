4848a4fe6e518e3c0245e343c5e54d68f038bc8d
Tom Silver <tsilver@theaiinstitute.com>
2024-01-12 14:28:42 -0500

    nvm

--- predicators/envs/spot_env.py
+++ predicators/envs/spot_env.py
@@ -207,7 +207,6 @@ class SpotRearrangementEnv(BaseEnv):
         # parts of the state during execution.
         self._strips_operators: Set[STRIPSOperator] = set()
         self._current_task_goal_reached = False
-        self._last_action: Optional[Action] = None
 
         # Create constant objects.
         self._spot_object = Object("robot", _robot_type)
@@ -372,14 +371,12 @@ class SpotRearrangementEnv(BaseEnv):
                                     f"was encountered. Trying again.\n{e}")
         self._current_observation = self._current_task.init_obs
         self._current_task_goal_reached = False
-        self._last_action = None
         return self._current_task.init_obs
 
     def step(self, action: Action) -> Observation:
         """Override step() because simulate() is not implemented."""
         assert isinstance(action.extra_info, (list, tuple))
         action_name, action_objs, action_fn, action_fn_args = action.extra_info
-        self._last_action = action
         # The extra info is (action name, objects, function, function args).
         # The action name is either an operator name (for use with nonpercept
         # predicates) or a special name. See below for the special names.
@@ -547,52 +544,6 @@ class SpotRearrangementEnv(BaseEnv):
         gripper_open_percentage = get_robot_gripper_open_percentage(
             self._robot)
         robot_pos = self._localizer.get_last_robot_pose()
-
-        # Hack to deal with the difficulty of reliably seeing objects after
-        # sweeping them. If we just finished sweeping some object(s) but we
-        # don't immediately see them afterwards, then ask the user to indicate
-        # whether each object ended up in the container. If so, then update
-        # the observation so that the swept object is inside the container.
-        # Otherwise do nothing and let the lost object dance proceed.
-        if self._last_action is not None:
-            assert isinstance(self._last_action.extra_info, (list, tuple))
-            op_name, op_objects, _, _ = self._last_action.extra_info
-            if op_name == "SweepTwoObjectsIntoContainer":
-                swept_objects: Set[Object] = set(op_objects[2:4])
-                container: Optional[Object] = op_objects[-1]
-            elif op_name == "SweepIntoContainer":
-                swept_objects = {op_objects[2]}
-                container = op_objects[-1]
-            else:
-                swept_objects = set()
-                container = None
-            static_feats = load_spot_metadata()["static-object-features"]
-            for swept_object in swept_objects:
-                if swept_object not in all_objects_in_view:
-                    assert container is not None
-                    assert container in all_objects_in_view
-                    while True:
-                        msg = (f"\nATTENTION! The {swept_object.name} was not "
-                               "seen after sweeping. Is it now in the "
-                               f"{container.name}? [y/n]\n")
-                        response = utils.prompt_user(msg)
-                        if response == "y":
-                            # Update the pose to be inside the container.
-                            container_pose = all_objects_in_view[container]
-                            # Calculate the z pose of the swept object.
-                            height = static_feats[swept_object.name]["height"]
-                            swept_object_z = container_pose.z + height / 2
-                            swept_pose = math_helpers.SE3Pose(
-                                x=container_pose.x,
-                                y=container_pose.y,
-                                z=swept_object_z,
-                                rot=container_pose.rot)
-                            all_objects_in_view[swept_object] = swept_pose
-                            objects_in_any_view_except_back.add(swept_object)
-                            break
-                        if response == "n":
-                            break
-
         # Prepare the non-percepts.
         nonpercept_preds = self.predicates - self.percept_predicates
         assert all(a.predicate in nonpercept_preds for a in ground_atoms)
@@ -800,7 +751,7 @@ class SpotRearrangementEnv(BaseEnv):
                                          y=0.0,
                                          z=0.75,
                                          rot=math_helpers.Quat.from_pitch(
-                                             np.pi / 3))
+                                             np.pi / 4))
         move_hand_to_relative_pose(self._robot, hand_pose)
         detections, artifacts = init_search_for_objects(
             self._robot,
@@ -842,7 +793,7 @@ _ONTOP_SURFACE_BUFFER = 0.48
 _INSIDE_SURFACE_BUFFER = 0.1
 _REACHABLE_THRESHOLD = 0.925  # slightly less than length of arm
 _REACHABLE_YAW_THRESHOLD = 0.95  # higher better
-_CONTAINER_SWEEP_READY_BUFFER = 0.5
+_CONTAINER_SWEEP_READY_BUFFER = 0.25
 _ROBOT_SWEEP_READY_TOL = 0.25
 
 ## Types
@@ -964,6 +915,12 @@ def _not_inside_any_container_classifier(state: State,
         if _inside_classifier(state, [obj_in, container]):
             return False
     return True
+
+
+def _fits_inside_classifier(state: State, objects: Sequence[Object]) -> bool:
+    # Create a hypothetical state where the contained object is positioned
+    # perfectly inside the container object and see whether Inside holds.
+    
 
 
 def in_hand_view_classifier(state: State, objects: Sequence[Object]) -> bool:
@@ -1096,9 +1053,15 @@ def _get_highest_surface_object_is_on(obj: Object,
     return highest_surface
 
 
-def _container_adjacent_to_surface_for_sweeping(container: Object,
-                                                surface: Object,
-                                                state: State) -> bool:
+def _container_ready_for_sweeping_classifier(
+        state: State, objects: Sequence[Object]) -> bool:
+    container, target = objects
+
+    # Compute the expected x, y position based on the parameters for placing
+    # next to the object that the target is on.
+    surface = _get_highest_surface_object_is_on(target, state)
+    if surface is None:
+        return False
 
     surface_x = state.get(surface, "x")
     surface_y = state.get(surface, "y")
@@ -1117,19 +1080,6 @@ def _container_adjacent_to_surface_for_sweeping(container: Object,
     return np.sqrt(
         (expected_x - container_x)**2 +
         (expected_y - container_y)**2) <= _CONTAINER_SWEEP_READY_BUFFER
-
-
-def _container_ready_for_sweeping_classifier(
-        state: State, objects: Sequence[Object]) -> bool:
-    container, target = objects
-
-    # Compute the expected x, y position based on the parameters for placing
-    # next to the object that the target is on.
-    surface = _get_highest_surface_object_is_on(target, state)
-    if surface is None:
-        return False
-    return _container_adjacent_to_surface_for_sweeping(container, surface,
-                                                       state)
 
 
 def _is_placeable_classifier(state: State, objects: Sequence[Object]) -> bool:
@@ -1170,10 +1120,13 @@ def _robot_ready_for_sweeping_classifier(state: State,
 def _get_sweeping_surface_for_container(container: Object,
                                         state: State) -> Optional[Object]:
     if container.is_instance(_container_type):
-        for surface in state.get_objects(_immovable_object_type):
-            if _container_adjacent_to_surface_for_sweeping(
-                    container, surface, state):
-                return surface
+        for candidate_surface in state.get_objects(_immovable_object_type):
+            for target_obj in state.get_objects(_movable_object_type):
+                if not _on_classifier(state, [target_obj, candidate_surface]):
+                    continue
+                if _container_ready_for_sweeping_classifier(
+                        state, [container, target_obj]):
+                    return candidate_surface
     return None
 
 
@@ -1185,6 +1138,8 @@ _TopAbove = Predicate("TopAbove", [_base_object_type, _base_object_type],
                       _top_above_classifier)
 _Inside = Predicate("Inside", [_movable_object_type, _container_type],
                     _inside_classifier)
+_FitsInside = Predicate("FitsInside", [_movable_object_type, _container_type],
+                        _fits_inside_classifier)
 # NOTE: use this predicate instead if you want to disable inside checking.
 _FakeInside = Predicate(_Inside.name, _Inside.types,
                         _create_dummy_predicate_classifier(_Inside))
@@ -1390,6 +1345,7 @@ def _create_operators() -> Iterator[STRIPSOperator]:
         LiftedAtom(_Holding, [robot, held]),
         LiftedAtom(_Reachable, [robot, container]),
         LiftedAtom(_IsPlaceable, [held]),
+        LiftedAtom(_FitsInside, [held, container]),
     }
     add_effs = {
         LiftedAtom(_Inside, [held, container]),
@@ -1421,6 +1377,7 @@ def _create_operators() -> Iterator[STRIPSOperator]:
         LiftedAtom(_HandEmpty, [robot]),
         LiftedAtom(_On, [held, surface]),
         LiftedAtom(_NotHolding, [robot, held]),
+        LiftedAtom(_FitsInside, [held, container]),
     }
     del_effs = {
         LiftedAtom(_Holding, [robot, held]),
