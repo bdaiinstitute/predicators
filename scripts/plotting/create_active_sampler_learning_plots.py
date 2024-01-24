01669aaf1dfd58da06315663b9d5dd3356a2ed5f
Tom Silver <tomssilver@gmail.com>
2024-01-24 14:46:31 -0500

    wip

--- scripts/plotting/create_active_sampler_learning_plots.py
+++ scripts/plotting/create_active_sampler_learning_plots.py
@@ -135,6 +135,24 @@ PLOT_GROUPS = {
         ("MAPLE-Q", "silver", lambda df: df["EXPERIMENT_ID"].apply(
             lambda v: "spot_sweeping_sim-maple_q" in v)),
     ],
+    "Cleanup Playroom (Yogurt Only)": [
+        ("Planning Progress", "green", lambda df: df["EXPERIMENT_ID"].apply(
+            lambda v: "spot_sweeping_sim_yogurt_only-planning_progress_explore" in v)),
+        ("Task Repeat", "orange", lambda df: df["EXPERIMENT_ID"].apply(
+            lambda v: "spot_sweeping_sim_yogurt_only-task_repeat_explore" in v)),
+        ("Competence Gradient", "yellow", lambda df: df["EXPERIMENT_ID"].apply(
+            lambda v: "spot_sweeping_sim_yogurt_only-competence_gradient" in v)),
+        ("Fail Focus", "red", lambda df: df["EXPERIMENT_ID"].apply(
+            lambda v: "spot_sweeping_sim_yogurt_only-success_rate_explore_ucb" in v)),
+        ("Task-Relevant", "purple", lambda df: df["EXPERIMENT_ID"].apply(
+            lambda v: "spot_sweeping_sim_yogurt_only-random_score_explore" in v)),
+        ("Random Skills", "blue", lambda df: df["EXPERIMENT_ID"].apply(
+            lambda v: "spot_sweeping_sim_yogurt_only-random_nsrts_explore" in v)),
+        ("Skill Diversity", "pink", lambda df: df["EXPERIMENT_ID"].apply(
+            lambda v: "spot_sweeping_sim_yogurt_only-skill_diversity" in v)),
+        ("MAPLE-Q", "silver", lambda df: df["EXPERIMENT_ID"].apply(
+            lambda v: "spot_sweeping_si_yogurt_only-maple_q" in v)),
+    ],
 }
 
 # If True, add (0, 0) to every plot.
