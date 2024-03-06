8e587843d7b77938757c82ed557cae32231a8c86
Tom Silver <tomssilver@gmail.com>
2024-03-06 15:14:03 -0500

    wip

--- scripts/bagel_oven_equidiff/bagel_oven_predicate_hacking.py
+++ scripts/bagel_oven_equidiff/bagel_oven_predicate_hacking.py
@@ -5,24 +5,7 @@ import numpy as np
 from predicators import utils
 import imageio.v2 as iio
 
-dirpath =  Path("/Users/tom/Desktop") / "equidiff"
-filepath = dirpath / "data_teleop_oven_full_x58.hdf5"
-assert filepath.exists()
-
-demo_num = 1
-
-f = h5py.File(filepath, 'r')
-dataset = f['dataset']
-demo = dataset[f'demo_{demo_num}']
-actions = demo["action"]
-voxels = demo["agentview_voxel"]
-eef_pos = demo["robot0_eef_pos"]
-eef_quat = demo["robot0_eef_quat"]
-gripper_qpos = demo["robot0_gripper_qpos"]
-
-imgs = []
-for t in range(len(voxels)):
-    voxel_map = np.swapaxes(voxels[t], 0, -1)
+def voxel_map_to_img(voxel_map):
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
     # X/Y
@@ -66,44 +49,35 @@ for t in range(len(voxels)):
     # Show the plot
     img = utils.fig2data(fig, dpi=150)
     plt.close()
-    imgs.append(img)
-
-video_path = dirpath / f"bagel_oven_viz_demo{demo_num}.mp4"
-iio.mimsave(video_path, imgs, fps=10)
-print(f"Wrote out to {video_path}")
+    return img
 
 
-#### From ChatGPT ####
+def create_voxel_map_video(demo_num):
 
-# # Assuming `voxel_map` is your (4, 64, 64, 64) array
-# # Rearrange the array to (64, 64, 64, 4) for RGBA to be the last dimension
-# voxel_map = np.transpose(voxel_map, (1, 2, 3, 0))
+    dirpath =  Path("/Users/tom/Desktop") / "equidiff"
+    filepath = dirpath / "data_teleop_oven_full_x58.hdf5"
+    assert filepath.exists()
 
-# # Create a figure and 3D axis
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
+    f = h5py.File(filepath, 'r')
+    dataset = f['dataset']
+    demo = dataset[f'demo_{demo_num}']
+    voxels = demo["agentview_voxel"]
 
-# # Prepare arrays to hold voxel coordinates and colors
-# x_coords, y_coords, z_coords, colors = [], [], [], []
+    # TODO: use?
+    eef_pos = demo["robot0_eef_pos"]
+    eef_quat = demo["robot0_eef_quat"]
+    gripper_qpos = demo["robot0_gripper_qpos"]
 
-# # Iterate over each voxel
-# for x in range(voxel_map.shape[0]):
-#     for y in range(voxel_map.shape[1]):
-#         for z in range(voxel_map.shape[2]):
-#             # Check if the voxel is not completely transparent (alpha channel is not 0)
-#             if voxel_map[x, y, z, 3] > 0:
-#                 x_coords.append(x)
-#                 y_coords.append(y)
-#                 z_coords.append(z)
-#                 colors.append(voxel_map[x, y, z] / 255)  # Extract RGB, ignore Alpha
+    imgs = []
 
-# # Scatter plot (use 'c' for color)
-# ax.scatter(x_coords, y_coords, z_coords, c=colors)
+    for t in range(len(voxels)):
+        voxel_map = np.swapaxes(voxels[t], 0, -1)
+        img = voxel_map_to_img(voxel_map)
+        imgs.append(img)
 
-# # Set plot limits
-# ax.set_xlim([0, voxel_map.shape[0]])
-# ax.set_ylim([0, voxel_map.shape[1]])
-# ax.set_zlim([0, voxel_map.shape[2]])
+    video_path = dirpath / f"bagel_oven_viz_demo{demo_num}.mp4"
+    iio.mimsave(video_path, imgs, fps=10)
+    print(f"Wrote out to {video_path}")
 
-# # Show the plot
-# plt.show()
+if __name__ == "__main__":
+    create_voxel_map_video(demo_num=40)
